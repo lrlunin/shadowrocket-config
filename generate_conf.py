bdb2e219f2e386555ef409632a61e5f10de95828
lrlunin <lunin@campus.tu-berlin.de>
2024-10-17 17:26:40 +0200

    ignore domains_all

--- generate_conf.py
+++ generate_conf.py
@@ -11,28 +11,28 @@ def generate_config(input_dir, output_conf):
         config_file.write("# from community.lst\n")
         with open(join(input_dir, "community.lst"), "r") as community:
             for line in community:
-                config_file.write(f"DOMAIN-SUFFIX,{line.strip()},PROXY\n")
+                config_file.write(f"{"DOMAIN-SUFFIX" if "." in line else "DOMAIN-KEYWORD"},{line.strip()},PROXY\n")
 
-        config_file.write("# from domains_all.lst\n")
-        with open(join(input_dir, "domains_all.lst"), "r") as domains_all:
-            for line in domains_all:
-                config_file.write(f"DOMAIN-SUFFIX,{line.strip()},PROXY\n")
-
+        # ignore since too many
+        # config_file.write("# from domains_all.lst\n")
+        # with open(join(input_dir, "domains_all.lst"), "r") as domains_all:
+        #     for line in domains_all:
+        #         config_file.write(f"{"DOMAIN-SUFFIX" if "." in line else "DOMAIN-KEYWORD"},{line.strip()},PROXY\n")
 
         config_file.write("# from ooni_domains.lst\n")
         with open(join(input_dir, "ooni_domains.lst"), "r") as ooni_domains:
             for line in ooni_domains:
-                config_file.write(f"DOMAIN-SUFFIX,{line.strip()},PROXY\n")
+                config_file.write(f"{"DOMAIN-SUFFIX" if "." in line else "DOMAIN-KEYWORD"},{line.strip()},PROXY\n")
 
 
         with open(join(input_dir, "user_custom.lst"), "r") as custom:
             for line in custom:
                 config_file.write(f"{"DOMAIN-SUFFIX" if "." in line else "DOMAIN-KEYWORD"},{line.strip()},PROXY\n")
 
-        # ignore since too many
-        # with open(join(input_dir, "ipsum.lst"), "r") as ip_networks:
-        #     for line in ip_networks:
-        #         config_file.write(f"IP-CIDR,{line.strip()},PROXY\n")
+        
+        with open(join(input_dir, "ipsum.lst"), "r") as ip_networks:
+            for line in ip_networks:
+                config_file.write(f"IP-CIDR,{line.strip()},PROXY\n")
 
 
         config_file.writelines(("\n\n# Финальное правило маршрутизации\n", "FINAL,DIRECT\n"))
